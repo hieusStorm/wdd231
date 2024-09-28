0f5ecdeec2443166db877dff1e11554f40d116a1
hieusStorm <hyrum555@gmail.com>
2024-09-28 09:56:45 -0700

    formatting for buttons added made inserting a list a thing

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -11,34 +11,72 @@ navButton.addEventListener("click", () => {
 async function retrieveMembers() {
     let response = await fetch("https://hieusstorm.github.io/wdd231/chamber/data/members.json");
     let data = await response.json();
-    displayMembers(data.members);
+    displayMembers(data.members, "grid");
+    return data.members;
 }
-retrieveMembers();
 
-const displayMembers = (members) => {
-    const gridArea = document.querySelector("#memberArea");
-    members.forEach(member => {
-        const section = document.createElement("section");
-        const img = document.createElement("img");
-        const addressTag = document.createElement("p");
-        const numberTag = document.createElement("p");
-        const memberUrl = document.createElement("a");
+const memberData = retrieveMembers();
 
-        img.setAttribute("src", member.imgUrl);
-        img.setAttribute("alt", member.imgName);
+const displayMembers = (members, type) => {
+    const memberArea = document.querySelector("#memberArea");
+    if (type == "grid") {
+        members.forEach(member => {
+            const section = document.createElement("section");
+            const img = document.createElement("img");
+            const addressTag = document.createElement("p");
+            const numberTag = document.createElement("p");
+            const memberUrl = document.createElement("a");
 
-        addressTag.innerHTML = member.address;
-        numberTag.innerHTML = member.phoneNumber;
+            img.setAttribute("src", member.imgUrl);
+            img.setAttribute("alt", member.imgName);
 
-        memberUrl.setAttribute("href", member.url);
-        memberUrl.setAttribute("target", "_blank");
-        memberUrl.innerText = member.url;
+            addressTag.innerHTML = member.address;
+            numberTag.innerHTML = member.phoneNumber;
 
-        section.appendChild(img);
-        section.appendChild(addressTag);
-        section.appendChild(numberTag);
-        section.appendChild(memberUrl);
+            memberUrl.setAttribute("href", member.url);
+            memberUrl.setAttribute("target", "_blank");
+            memberUrl.innerText = member.url;
 
-        gridArea.appendChild(section);
-    });
+            section.appendChild(img);
+            section.appendChild(addressTag);
+            section.appendChild(numberTag);
+            section.appendChild(memberUrl);
+
+            memberArea.appendChild(section);
+        });
+    } else {
+        const table = document.createElement("table");
+        members.forEach(member => {
+            const tr = document.createElement("tr");
+            const memberName = document.createElement("td");
+            const address = document.createElement("td");
+            const numberTag = document.createElement("td");
+            const memberUrl = document.createElement("td");
+            const memberLink = document.createElement("a");
+
+            memberName.innerHTML = member.name;
+            address.innerHTML = member.address;
+            numberTag.innerHTML = member.phoneNumber;
+
+            memberLink.setAttribute("href", member.url);
+            memberLink.setAttribute("target", "_blank");
+            memberLink.innerHTML = member.url;
+
+            memberUrl.appendChild(memberLink);
+
+            tr.appendChild(memberName);
+            tr.appendChild(address);
+            tr.appendChild(numberTag);
+            tr.appendChild(memberUrl);
+
+            table.appendChild(tr);
+        });
+    }
 }
+
+//buttons to change display style
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+gridButton.addEventListener("click", displayMembers(memberData, "grid"));
+listButton.addEventListener("click", displayMembers(memberData, "list"));
